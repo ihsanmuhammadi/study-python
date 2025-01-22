a9e24ca6c2ff3d87692f55fa766fe24be5b5d173
Ihsan Muhammad Iqbal <ihsanmiqbal24@gmail.com>
2025-01-22 17:30:13 +0700

    first-commit

---
+++ 1_hello_world.py
@@ -0,0 +1,6 @@
+print("Hello, World!")
+
+# Indentation - using blocks (space/tab)
+x = 1
+if x == 1:
+    print("x is 1")
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # study-python
 Sharing my journey of learning python
+Source: https://www.learnpython.org/en/Welcome
